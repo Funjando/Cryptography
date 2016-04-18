16994a7f9388f48c41a299cf086d8d6faeae28b0
Funjando <andojuuichi@gmail.com>
2016-04-18 14:41:30 -0400

    Updated from Brython Server: 4/18/2016 2:42:03 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Andreas Moberg
+Credit: Payton (for mad math skillz)
 
 Assignment:
 
@@ -10,3 +10,45 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+keystroke=input("Enter e to encrypt, d to decrypt, or q to quit:")
+
+cryptm=[]
+
+
+if keystroke=="e":
+    mess=input("Message:")
+    key=input("Key:")
+    k=len(key)
+    m=len(mess)
+    
+    if m>k:
+        count=key*int((m-(m%k))/k)
+        trun=key[0:(m%k)]
+        newkey=count+trun
+    elif k>m:
+        newkey=key[0:m] 
+        print(newkey, message)
+   
+    for x in mess:
+        for y in key:
+            cryptm.append(associations.find(x)+associations.find(y))
+    for x in cryptm:
+        print(associations[x])
+    
+    
+    
+elif keystroke=="d":
+    mess=input("Message:")
+    key=input("Key:")
+    
+elif keystroke=="q":
+    ...
+    print("Goodbye!")
+    
+else:
+    print("Did not understand command, try again.")
+
+
+
+
