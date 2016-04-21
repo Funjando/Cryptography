8e3db11337cac38b4467e9601e824cb0c267ea07
Funjando <andojuuichi@gmail.com>
2016-04-20 22:24:59 -0400

    Updated from Brython Server: 4/20/2016 10:25:45 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
 Author: Andreas Moberg
-Credit: Payton (for mad math skillz), Ashwini Chauswerry, UncleZiev
+Credit: Payton (for mad math skillz), Ashwini Chauswerry, UncleZiev, Alejandro
 
 Assignment:
 
