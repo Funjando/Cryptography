4b640d22594c39cfbb2b2a8ae8c79fb759b42b96
Funjando <andojuuichi@gmail.com>
2016-04-21 21:42:15 -0400

    Updated from Brython Server: 4/21/2016 9:43:05 PM

--- cryptography.py
+++ cryptography.py
@@ -8,95 +8,97 @@ Assignment:
 Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+
+infinity=1
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
 #print(len(associations))
 #85
+while infinity>0:
+    keystroke=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
-keystroke=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    quit=False
 
-quit=False
+    if keystroke not in ["e", "d", "q"]:
+        print("Did not understand command, try again. ")
 
-if keystroke not in ["e", "d", "q"]:
-    print("Did not understand command, try again. ")
-
-if keystroke=="e":
-    mess=input("Message: ")
-    key=input("Key: ")
-    k=len(key)
-    m=len(mess)
-    cryptm=[]
-    cryptk=[]
-    encrypt=[]
-    tencrypt=[]
+    if keystroke=="e":
+        mess=input("Message: ")
+        key=input("Key: ")
+        k=len(key)
+        m=len(mess)
+        cryptm=[]
+        cryptk=[]
+        encrypt=[]
+        tencrypt=[]
     
-    if m>k:
-        count=key*int((m-(m%k))/k)
-        trun=key[0:(m%k)]
-        key=count+trun
-    elif k>m:
-        key=key[0:m] 
-        #print(newkey, message)
+        if m>k:
+            count=key*int((m-(m%k))/k)
+            trun=key[0:(m%k)]
+            key=count+trun
+        elif k>m:
+            key=key[0:m] 
+            #print(newkey, message)
    
-    for x in mess:
-        cryptm.append(associations.find(x))
-    for x in key:
-        cryptk.append(associations.find(x))
+        for x in mess:
+            cryptm.append(associations.find(x))
+        for x in key:
+            cryptk.append(associations.find(x))
     
     
-    encrypt=[sum(x) for x in zip(cryptm, cryptk)]
+        encrypt=[sum(x) for x in zip(cryptm, cryptk)]
 
     
-#    for x in encrypt:
-#       if x>85:
-#            x.replace(x, x-85)
+#       for x in encrypt:
+#           if x>85:
+#               x.replace(x, x-85)
     
     
-    for x in encrypt:
-        tencrypt.append(associations[x%85])
-    for x in tencrypt:
-        print(x, end="")
+        for x in encrypt:
+            tencrypt.append(associations[x%85])
+        for x in tencrypt:
+            print(x, end="")
     
     
-if keystroke=="d":
-    mess=input("Message: ")
-    key=input("Key: ")
-    k=len(key)
-    m=len(mess)
-    mes=[]
-    cryptm=[]
-    cryptk=[]
-    decrypt=[]
-    tdecrypt=[]
+    if keystroke=="d":
+        mess=input("Message: ")
+        key=input("Key: ")
+        k=len(key)
+        m=len(mess)
+        mes=[]
+        cryptm=[]
+        cryptk=[]
+        decrypt=[]
+        tdecrypt=[]
     
-    if m>k:
-        count=key*int((m-(m%k))/k)
-        trun=key[0:(m%k)]
-        newkey=count+trun
-    elif k>m:
-        newkey=key[0:m] 
-        print(newkey, message)
+        if m>k:
+            count=key*int((m-(m%k))/k)
+            trun=key[0:(m%k)]
+            newkey=count+trun
+        elif k>m:
+            newkey=key[0:m] 
+            print(newkey, message)
    
     
-    for x in mess:
-        cryptm.append(associations.find(x))
-    for x in newkey:
-        cryptk.append(associations.find(x))
+        for x in mess:
+            cryptm.append(associations.find(x))
+        for x in newkey:
+            cryptk.append(associations.find(x))
         
     
     
-    decrypt=[x-y for x, y in zip(cryptm, cryptk)]
+        decrypt=[x-y for x, y in zip(cryptm, cryptk)]
     
     
-    for x in decrypt:
-        tdecrypt.append(associations[x%85])
-    for x in tdecrypt:
-        print(x, end="")
+        for x in decrypt:
+            tdecrypt.append(associations[x%85])
+        for x in tdecrypt:
+            print(x, end="")
         
     
-if keystroke=="q":
-    quit=True
-    print("Goodbye!")
+    if keystroke=="q":
+        quit=True
+        print("Goodbye!")
 
 
 
